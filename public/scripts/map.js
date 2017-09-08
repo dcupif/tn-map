7bbcdcbc42d703038c84cd950e12179674196e10
GuillaumeHaben <pyxouze@gmail.com>
2017-09-08 05:35:24 -0400

    Add style, create folders

---
+++ public/scripts/map.js
@@ -0,0 +1,297 @@
+function initMap() {
+    		var styledMapType = new google.maps.StyledMapType(
+    			[
+				  {
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#f5f5f5"
+				      }
+				    ]
+				  },
+				  {
+				    "elementType": "labels.icon",
+				    "stylers": [
+				      {
+				        "visibility": "off"
+				      }
+				    ]
+				  },
+				  {
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#616161"
+				      }
+				    ]
+				  },
+				  {
+				    "elementType": "labels.text.stroke",
+				    "stylers": [
+				      {
+				        "color": "#f5f5f5"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "administrative.land_parcel",
+				    "stylers": [
+				      {
+				        "visibility": "off"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "administrative.land_parcel",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#bdbdbd"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "administrative.neighborhood",
+				    "stylers": [
+				      {
+				        "visibility": "off"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "poi",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#eeeeee"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "poi",
+				    "elementType": "labels.text",
+				    "stylers": [
+				      {
+				        "visibility": "off"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "poi",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#757575"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "poi.business",
+				    "stylers": [
+				      {
+				        "visibility": "off"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "poi.park",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#e5e5e5"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "poi.park",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#9e9e9e"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#ffffff"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road",
+				    "elementType": "labels",
+				    "stylers": [
+				      {
+				        "visibility": "off"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road",
+				    "elementType": "labels.icon",
+				    "stylers": [
+				      {
+				        "visibility": "off"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.arterial",
+				    "stylers": [
+				      {
+				        "visibility": "off"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.arterial",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#757575"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.highway",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#dadada"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.highway",
+				    "elementType": "labels",
+				    "stylers": [
+				      {
+				        "visibility": "off"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.highway",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#616161"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.local",
+				    "stylers": [
+				      {
+				        "visibility": "off"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "road.local",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#9e9e9e"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "transit",
+				    "stylers": [
+				      {
+				        "visibility": "off"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "transit.line",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#e5e5e5"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "transit.station",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#eeeeee"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "water",
+				    "elementType": "geometry",
+				    "stylers": [
+				      {
+				        "color": "#c9c9c9"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "water",
+				    "elementType": "labels.text",
+				    "stylers": [
+				      {
+				        "visibility": "off"
+				      }
+				    ]
+				  },
+				  {
+				    "featureType": "water",
+				    "elementType": "labels.text.fill",
+				    "stylers": [
+				      {
+				        "color": "#9e9e9e"
+				      }
+				    ]
+				  }
+				],
+
+            {name: 'Styled Map'});
+
+			var uluru = {lat: -25.363, lng: 131.044};
+			var map = new google.maps.Map(document.getElementById('map'), {
+				zoom: 4,
+				center: uluru
+			});
+			map.mapTypes.set('styled_map', styledMapType);
+        	map.setMapTypeId('styled_map');
+
+			// Try HTML5 geolocation.
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(function(position) {
+				var pos = {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude
+				};
+				map.setCenter(pos);
+				var marker = new google.maps.Marker({
+					position: pos,
+					map: map,
+					icon: 'images/user-placeholder.png'
+				});
+			}, function() {
+				handleLocationError(true, infoWindow, map.getCenter());
+			});
+			} else {
+				// Browser doesn't support Geolocation
+				handleLocationError(false, infoWindow, map.getCenter());
+			}
+
+
+			function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+				infoWindow.setPosition(pos);
+				infoWindow.setContent(browserHasGeolocation ?
+				'Error: The Geolocation service failed.' :
+				'Error: Your browser doesn\'t support geolocation.');
+			}
+    	}
+
+    	//user-placeholder.png
