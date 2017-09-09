9eb72f57b184fbbd0c0b3ee5226c883971632d14
Caroline BOYER <caroline.boyer@telecomnancy.eu>
2017-09-09 16:57:51 -0400

    Design pages - v1

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.render('login', {
+        title: 'TELECOM Nancy - Visitor Map'
+    });
 });
 
 module.exports = router;
