991be10b4b8631c53bce91e7d3ff6578651e09f8
Damien Cupif <damien.cupif@gmail.com>
2017-09-09 00:09:26 -0400

    Add login page + route without logic

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'TELECOM Nancy - Visitor Map' });
 });
 
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { user: req.user, message: req.flash('error') });
+});
+
 module.exports = router;
