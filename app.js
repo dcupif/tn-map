407a53d3f8783e9bed2d33ad6398a3f7c5675d37
GuillaumeHaben <pyxouze@gmail.com>
2017-09-11 01:45:06 -0400

    Passport working (with password)

--- app.js
+++ app.js
@@ -9,33 +9,34 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var utils = require('./utils')
 
-var index = require('./routes/index');
+var index = require('./routes/index')(passport, issueToken);
 
 /* Fake, in-memory database of users */
 
 var users = [
-  { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
-, { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
 ];
 
 function findById(id, fn) {
-var idx = id - 1;
-if (users[idx]) {
-  fn(null, users[idx]);
-} else {
-  fn(new Error('User ' + id + ' does not exist'));
-}
+  var idx = id - 1;
+  if (users[idx]) {
+      fn(null, users[idx]);
+  } else {
+      fn(new Error('User ' + id + ' does not exist'));
+  }
 }
 
 function findByUsername(username, fn) {
-for (var i = 0, len = users.length; i < len; i++) {
-  var user = users[i];
-  if (user.username === username) {
-    return fn(null, user);
+  for (var i = 0, len = users.length; i < len; i++) {
+      var user = users[i];
+      if (user.username === username) {
+          return fn(null, user);
+      }
   }
-}
-return fn(null, null);
+  return fn(null, null);
 }
 
 /* Fake, in-memory database of remember me tokens */
@@ -43,18 +44,16 @@ return fn(null, null);
 var tokens = {}
 
 function consumeRememberMeToken(token, fn) {
-var uid = tokens[token];
-// invalidate the single-use token
-delete tokens[token];
-return fn(null, uid);
+  var uid = tokens[token];
+  // invalidate the single-use token
+  delete tokens[token];
+  return fn(null, uid);
 }
 
 function saveRememberMeToken(token, uid, fn) {
-tokens[token] = uid;
-return fn();
+  tokens[token] = uid;
+  return fn();
 }
-
-
 
 // Passport session setup.
 //   To support persistent login sessions, Passport needs to be able to
@@ -62,38 +61,36 @@ return fn();
 //   this will be as simple as storing the user ID when serializing, and finding
 //   the user by ID when deserializing.
 passport.serializeUser(function(user, done) {
-done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
-findById(id, function (err, user) {
-  done(err, user);
+  findById(id, function (err, user) {
+    done(err, user);
+  });
 });
-});
-
 
 // Use the LocalStrategy within Passport.
 //   Strategies in passport require a `verify` function, which accept
-//   credentials (in this case, a username and password), and invoke a callback
+//   credentials (in this case, a username), and invoke a callback
 //   with a user object.  In the real world, this would query a database;
 //   however, in this example we are using a baked-in set of users.
 passport.use(new LocalStrategy(
-function(username, password, done) {
-  // asynchronous verification, for effect...
-  process.nextTick(function () {
-    
-    // Find the user by username.  If there is no user with the given
-    // username, or the password is not correct, set the user to `false` to
-    // indicate failure and set a flash message.  Otherwise, return the
-    // authenticated `user`.
-    findByUsername(username, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
-      if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
-      return done(null, user);
-    })
-  });
-}
+  function(username, password, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      // Find the user by username.  If there is no user with the given
+      // username, set the user to `false` to
+      // indicate failure and set a flash message.  Otherwise, return the
+      // authenticated `user`.
+      findByUsername(username, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+        return done(null, user);
+      })
+    });
+  }
 ));
 
 // Remember Me cookie strategy
@@ -101,27 +98,27 @@ function(username, password, done) {
 //   token was originally issued to.  The token is single-use, so a new
 //   token is then issued to replace it.
 passport.use(new RememberMeStrategy(
-function(token, done) {
-  consumeRememberMeToken(token, function(err, uid) {
-    if (err) { return done(err); }
-    if (!uid) { return done(null, false); }
-    
-    findById(uid, function(err, user) {
+  function(token, done) {
+    consumeRememberMeToken(token, function(err, uid) {
       if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      return done(null, user);
+      if (!uid) { return done(null, false); }
+      
+      findById(uid, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        return done(null, user);
+      });
     });
-  });
-},
-issueToken
+  },
+  issueToken
 ));
 
 function issueToken(user, done) {
-var token = utils.randomString(64);
-saveRememberMeToken(token, user.id, function(err) {
-  if (err) { return done(err); }
-  return done(null, token);
-});
+  var token = utils.randomString(64);
+  saveRememberMeToken(token, user.id, function(err) {
+    if (err) { return done(err); }
+    return done(null, token);
+  });
 }
 
 var app = express();
@@ -156,7 +153,7 @@ app.use('/', index);
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  next(err);
+  res.redirect('/');
 });
 
 // error handler
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+module.exports = function(passport, issueToken) {
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', {
-        title: 'TELECOM Nancy - Visitor Map'
-    });
-});
+	/* GET home page. */
+	router.get('/', ensureAuthenticated, function(req, res, next) {
+	    res.render('index');
+	});
 
-/* GET login page. */
-router.get('/login', function(req, res, next) {
-  res.render('login', {
-  		user: req.user,
-  		title: 'Login Page',
-  		message: req.flash('error') });
-});
+	/* GET login page. */
+	router.get('/login', function(req, res, next) {
+	  res.render('login', {
+	  		user: req.user,
+	  		message: req.flash('error') });
+	});
 
-module.exports = router;
+	/* POST login page. */
+	router.post('/login', 
+	  passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+	  function(req, res, next) {
+	    // Issue a remember me cookie if the option was checked	  
+	    issueToken(req.user, function(err, token) {
+	      if (err) { return next(err); }
+	      res.cookie('remember_me', token, { path: '/', httpOnly: true, maxAge: 604800000 });
+	      return next();
+	    });
+	  },
+	  function(req, res) {
+	    res.redirect('/');
+	  });
+
+	/* GET logout page. */
+	router.get('/logout', function(req, res){
+	  // clear the remember me cookie when logging out
+	  res.clearCookie('remember_me');
+	  req.logout();
+	  res.redirect('/');
+	});
+
+	function ensureAuthenticated(req, res, next) {
+	  if (req.isAuthenticated()) { return next(); }
+	  res.redirect('/login')
+	}
+
+	return router;
+}
